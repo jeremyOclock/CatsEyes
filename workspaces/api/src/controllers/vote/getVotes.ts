1b1b9552497a5f8a50dc9cb97268915343b42786
jeremyOclock <jere.bs92@gmail.com>
2019-12-24 01:05:57 +0100

    feat: 🎸 GET on /vote
    permit to get cats sorted by their scores & pagination is doable with
    the limit and page params.

--- workspaces/api/src/controllers/vote/getVotes.ts
+++ workspaces/api/src/controllers/vote/getVotes.ts
@@ -1,5 +1,24 @@
 import { RequestHandler } from 'express';
+import Cat from '../../models/Cat';
 
-const getVotes: RequestHandler = (_, res) => res.send('from vote');
+const getVotes: RequestHandler = async (req, res) => {
+  let { page, limit } = req.query;
+
+  limit = +limit || 50;
+  page = +page ? limit * (+page - 1) : 0;
+
+  try {
+    const cats = await Cat.find({})
+      .skip(page)
+      .limit(limit)
+      .sort({ score: 'desc' });
+
+    if (!cats || !cats.length) throw null;
+
+    res.send(cats);
+  } catch {
+    res.status(404).send('No cat(s) found');
+  }
+};
 
 export default getVotes;
