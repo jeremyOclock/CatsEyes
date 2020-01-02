fd630ac845981e9d4af3cb7a0a0839c150ed4f22
jeremyOclock <jere.bs92@gmail.com>
2020-01-02 18:37:37 +0100

    feat: 🎸 Adding animations for ResultsCats

--- workspaces/client/src/components/ResultsCats/index.tsx
+++ workspaces/client/src/components/ResultsCats/index.tsx
@@ -1,10 +1,42 @@
 import React, { useEffect, useCallback, useRef } from 'react';
+import { useTransition, animated } from 'react-spring';
 import ScrollToTop from '../ScrollToTop';
 import './resultsCats.scss';
 import { Link } from 'react-router-dom';
 import { useSelector } from '../../hooks/useSelector';
 import { useDispatch } from 'react-redux';
 import { fetchData } from '../../redux/actions/feature/resultscats';
+import { ICat } from '@workspaces/api/src/models/Cat';
+
+const Cards: React.FC<{ cats: ICat[] }> = ({ cats }) => {
+  const transitions = useTransition(cats, cat => cat._id, {
+    unique: true,
+    trail: 50,
+    from: { opacity: 0, transform: 'scale(0)' },
+    enter: { opacity: 1, transform: 'scale(1)' },
+    leave: { opacity: 0, transform: 'scale(0)' }
+  });
+
+  return (
+    <>
+      {transitions.map(({ item, key, props }) => (
+        <animated.section
+          className="avatars-cats__container"
+          key={key}
+          style={props}
+        >
+          <i
+            className="avatars-cats__container__img"
+            style={{ backgroundImage: `url(${item.image})` }}
+          />
+          <span className="avatars-cats__container__score">
+            Score: {item.score}
+          </span>
+        </animated.section>
+      ))}
+    </>
+  );
+};
 
 const ResultsCats = () => {
   const { cats } = useSelector(state => state.resultsCats);
@@ -38,19 +70,7 @@ const ResultsCats = () => {
 
   return (
     <div className="avatars-cats">
-      {cats
-        ? cats.map(({ score, image }, i) => (
-            <section key={i} className="avatars-cats__container">
-              <i
-                className="avatars-cats__container__img"
-                style={{ backgroundImage: `url(${image})` }}
-              />
-              <span className="avatars-cats__container__score">
-                Score: {score}
-              </span>
-            </section>
-          ))
-        : null}
+      {cats && <Cards cats={cats} />}
       <Link to="/" className="avatars-cats__back-to-home">
         <i className="fas fa-angle-double-left avatars-cats__back-to-home__icon"></i>
       </Link>
